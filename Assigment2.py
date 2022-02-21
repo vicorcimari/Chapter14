3764d25cd7230ab86271da101b31a66691c6ddcf
Victor Corcimari <victorcorcimari@Victors-MacBook-Pro.local>
2022-02-21 17:15:05 -0600

    Assigment2.py got added

---
+++ Assigment2.py
@@ -0,0 +1,16 @@
+#Exercise_1
+my_string="Test Automation"
+#Type your answer here
+word_test="Test Automation"
+print(word_test[0:4])
+
+#Exercise_2
+my_string="Test Automation"
+#Type your answer here
+second_word="Test Automation"
+print(second_word[5:15])
+
+#Exercise_3
+my_list=[10,50,20,25,30]
+sliced_list=[10,50,20,25,30]
+print(sliced_list[1:3])
